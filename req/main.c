3dd7987552c0b5394c0259d00a4089e920ab31f8
satya-snigdha <161824764+satya-snigdha@users.noreply.github.com>
2024-05-17 08:30:58 +0000

    adding 2 nos

---
+++ req/main.c
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online C Compiler.
+                Code, Compile, Run and Debug C program online.
+Write your code in this editor and press "Run" button to compile and execute it.
+
+*******************************************************************************/
+
+#include <stdio.h>
+
+int main()
+{
+    int num1,num2;
+    int res;
+    printf("Enter 2 numbers");
+    scanf("%d %d",&num1,&num2);
+    res=num1+num2;
+    printf("Result %d",res);
+}
