c754103ed93ff1dcfefb9654f01218432db4bd2d
BTWITSAKRU <Akramcei78@gmail.com>
2024-05-17 09:20:22 +0000

    multiplication, calculator must support multiplication of two numbers  #1

--- req/main.c
+++ req/main.c
@@ -8,6 +8,13 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 #include <stdio.h>
 
+int multiplication(int num1, int num2)
+{
+    int result=0;
+    result=num1*num2;
+    return result;
+}
+
 int division(int num1, int num2)
 {
     int result=0;
